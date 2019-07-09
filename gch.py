0b48a1faaf09a85e96ba9a759faf30ae7fdf4509
Scstechr <teufelkonig@gmail.com>
2019-07-10 00:05:24 +0900

    alignments of arguments/options changed

--- gch.py
+++ gch.py
@@ -42,36 +42,36 @@ diffcmd = 'git diff --cached --ignore-all-space --ignore-blank-lines'
 logcmd =  'git log --stat --oneline --graph --decorate'
 
 # Explanation of the options showed in --help flag
-exp_i=f'Run initializer or not.'.ljust(38)+f'>Default:{defaults["init"]}'
+exp_c=f'Commit'
+exp_p=f'Push.'
+exp_i=f'Run initializer'.ljust(38)+f'>Default:{defaults["init"]}'
 exp_g=f'Path of dir that contains `.git`.'.ljust(38)+f'>Default:{defaults["gitpath"]}'
 exp_f=f'Path/Regex of staging file/dir.'.ljust(38)+f'>Default:{defaults["filepath"]}'
 exp_b=f'Commiting branch.'.ljust(38)+f'>Default:{defaults["branch"]}'
-exp_d=f'Detailed diff.'.ljust(38)+f'>Default:{defaults["verbose"]}'
+exp_v=f'Verbose option.'.ljust(38)+f'>Default:{defaults["verbose"]}'
 exp_l=f'Git log with option.'.ljust(38)+f'>Default:{defaults["log"]}'
-exp_c=f'Commit or not.'.ljust(38)+f'>Default:{defaults["commit"]}'
 exp_r=f'Reset all changes since last commit.'.ljust(38)+f'>Default:{defaults["reset"]}'
-exp_p=f'Push or not.'.ljust(38)+f'>Default:{defaults["push"]}'
 exp_e=f'Choose which remote repo.to push.'.ljust(38)+f'>Default:{defaults["remote"]}'
 exp_p2=f'Pull from <{defaults["remote"]}> <{defaults["branch"]}>.'.ljust(38)+f'>Default:False'
 exp_s=f'Save settings'.ljust(38)+f'>Default:False'
 exp_u=f'Update gch'.ljust(38)+f'>Default:False'
-exp_d2=f'Open diff tool'.ljust(38)+f'>Default:False'
+exp_d=f'Open diff tool'.ljust(38)+f'>Default:False'
 
 @click.command()
-@click.option('-i', '--init',     is_flag=defaults['init'],     help=exp_i)
-@click.option('-v', '--verbose',   is_flag=defaults['verbose'],   help=exp_d)
-@click.option('-l', '--log',      is_flag=defaults['log'],      help=exp_l)
 @click.option('-c', '--commit',   is_flag=defaults['commit'],   help=exp_c)
-@click.option('-r', '--remote',   default=defaults['remote'],    help=exp_e)
 @click.option('-p', '--push',     is_flag=defaults['push'],     help=exp_p)
+@click.option('-i', '--init',     is_flag=defaults['init'],     help=exp_i)
+@click.option('-v', '--verbose',   is_flag=defaults['verbose'],   help=exp_v)
+@click.option('-l', '--log',      is_flag=defaults['log'],      help=exp_l)
+@click.option('-r', '--remote',   default=defaults['remote'],    help=exp_e)
 @click.option('-g', '--gitpath',  default=defaults['gitpath'],  type=click.Path(exists=True), help=exp_g)
 @click.option('-f', '--filepath', default=defaults['filepath'], type=str, help=exp_f)
 @click.option('-b', '--branch',   default=defaults['branch'],   type=str, help=exp_b)
 @click.option('-s', '--save',     is_flag='False',              help=exp_s)
+@click.option('-d', '--diff',     is_flag=defaults['diff'],     type=str, help=exp_d)
+@click.option('-u', '--update',   is_flag=defaults['update'],   type=str, help=exp_u)
 @click.option('--reset',          is_flag=defaults['reset'],    type=str, help=exp_r)
 @click.option('--pull',           is_flag=defaults['pull'],     type=str, help=exp_p2)
-@click.option('-u', '--update',   is_flag=defaults['update'],   type=str, help=exp_u)
-@click.option('-d', '--diff',     is_flag=defaults['diff'],     type=str, help=exp_d2)
 def main(init,
          verbose,
          log,
