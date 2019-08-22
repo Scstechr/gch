9880387748ac4919a98af22219cd24d9a2a62013
Scstechr <teufelkonig@gmail.com>
2019-08-23 08:06:22 +0900

    remove clang information

--- src/version.py
+++ src/version.py
@@ -1,8 +1,9 @@
+import sys
+
 VERSION = '1.24'
-DATE = "2019-08-22 UTC"
+DATE = "2019-08-22 23:04:23 UTC"
 PYTHON_VERSION = '3.7.3'
 PLATFM_VERSION = 'Darwin-18.0.0-x86_64-i386-64bit'
-CLANG_VERSION = '4.0.1'
 PYINST_VERSION = '3.5'
 
 def ShortVersion(string):
@@ -14,10 +15,6 @@ def Version(string):
     # Python version
     version = PYTHON_VERSION
     print(f"\033[0m Python      :\033[0m", version)
-
-    # Clang version
-    version = CLANG_VERSION
-    print(f"\033[0m Clang    :\033[0m", version)
 
     # PyInstaller version
     version = PYINST_VERSION
