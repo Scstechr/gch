42a45ab86ef65056ec406a85552572fc24026837
Scstechr <teufelkonig@gmail.com>
2019-09-16 00:48:22 +0900

    v1.37

--- src/version.py
+++ src/version.py
@@ -4,8 +4,8 @@ import urllib.error as err
 from . import issues
 from .colors import R, G, Y, B, P, C, GR, BL, TH, IT, M
 
-VERSION = '1.36'
-DATE = "2019-09-14 15:19:10 UTC"
+VERSION = '1.37'
+DATE = "2019-09-15 15:32:32 UTC"
 PYTHON_VERSION = '3.7.3'
 PLATFM_VERSION = 'Darwin-18.0.0-x86_64-i386-64bit'
 PYINST_VERSION = '3.5'
