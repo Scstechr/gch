10021d8177233c40f261d6c00b91266354626b0b
Scstechr <teufelkonig@gmail.com>
2019-10-03 16:15:13 +0900

    v1.46 (fix bug)

--- src/version.py
+++ src/version.py
@@ -5,7 +5,7 @@ from . import issues
 from .colors import BL, M
 
 VERSION = '1.46'
-DATE = "2019-10-03 07:00:28 UTC"
+DATE = "2019-10-03 07:14:55 UTC"
 PYTHON_VERSION = '3.7.3'
 PLATFM_VERSION = 'Darwin-18.0.0-x86_64-i386-64bit'
 PYINST_VERSION = '3.5'
